f2c65bc77bf7e6d0780466c32cedc020826471c9
drobinin575 <drobinin575@gmail.com>
2022-07-31 18:26:43 +0300

    fixed validations, added indices to nickname and email

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,17 +1,3 @@
 class Question < ApplicationRecord
-  validate :custom_message
-
-  def custom_message
-    if body.blank?
-      errors.add(:_, "The question text cannot be blank.")
-    end
-
-    if body.length < 10
-      errors.add(:_, "The question text should contain minimum 10 symbols.")
-      end
-
-    if body.length > 280
-      errors.add(:_, "The question text should contain maximum 280 symbols.")
-    end
-  end
+  validates :body, presence: true, length: { in: 10..280 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
-  before_save :downcase_nickname
+  before_validation :downcase_nickname
 
   validates :email, :nickname, presence: true, uniqueness: true
   validates :email, format: {
     with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/
   }
-  validates :nickname, length: { in: 2..40, message: 'should contain 2-40 symbols' },
-            format: { with: /\A\w+\z/, message: 'only allows letters, digits and _' }
+  validates :nickname, length: { in: 2..40 }, format: { with: /\A\w+\z/ }
+
+  private
 
   def downcase_nickname
     nickname.downcase!
