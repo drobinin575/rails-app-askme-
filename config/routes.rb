066ab8072b6f483d8c4d018ea3a7442e47a87778
drobinin575 <drobinin575@gmail.com>
2022-08-16 11:38:26 +0300

    Revert "edited changing navbar color, added jquery"
    This reverts commit 9e7404142954b2c671663fb5299a28f360c8ac72.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     put 'hide', on: :member
   end
   resource :session, only: %i[new create destroy]
-  resources :users, param: :nickname, except: %i[index]
+  resources :users, param: :nickname, except: %i[index] do
+    put 'reset', on: :member
+  end
 end
