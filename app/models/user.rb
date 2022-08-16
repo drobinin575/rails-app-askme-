2183c18c83f8cfbc3712b84a0360a0ed77db6648
drobinin575 <drobinin575@gmail.com>
2022-08-16 17:58:37 +0300

    fixed deletion of user-author: added dependence

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   has_secure_password
   before_validation :downcase_nickname
 
+  has_many :questions, dependent: :delete_all
+  has_many :authored_questions, class_name: 'Question', foreign_key: 'author_id', dependent: :nullify
+
   validates :email, :nickname, presence: true, uniqueness: true
   validates :email, format: {
     with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/
   }
   validates :nickname, length: { in: 2..40 }, format: { with: /\A\w+\z/ }
-
-  has_many :questions, dependent: :delete_all
 
   def to_param
     nickname
