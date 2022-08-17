27b59730ae0a79027dd2dc2931fdecfa5781183d
drobinin575 <drobinin575@gmail.com>
2022-08-17 10:44:30 +0300

    replaced method find_by by find_by! for error 404 instead of error 500

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,6 @@ class UsersController < ApplicationController
   end
 
   def set_user
-    @user = User.find_by(nickname: params[:nickname])
+    @user = User.find_by!(nickname: params[:nickname])
   end
 end
